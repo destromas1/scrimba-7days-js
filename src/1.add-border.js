16def0aa3fd414181d3dd014b88729c1139bd704
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2020-06-03 00:20:32 +0200

    trainer's solution

--- src/1.add-border.js
+++ src/1.add-border.js
@@ -3,10 +3,26 @@ const addBorder = (array) => {
   const border = "*****";
 
   newArray.push(border);
-  newArray = newArray.concat(...array.map(val => `*${val}*`));
+  newArray = newArray.concat(...array.map((val) => `*${val}*`));
   newArray.push(border);
 
   return newArray;
 };
 
 export default addBorder;
+
+/*
+function addBorder(array) {
+  //  write code here.
+  const wall = "*".repeat(array[0].length + 2);
+
+  array.unshift(wall);
+  array.push(wall);
+
+  for (let i = 1; i < array.length - 1; i++) {
+    array[i] = "*".concat(array[i], "*");
+  }
+
+  return array;
+}
+*/
