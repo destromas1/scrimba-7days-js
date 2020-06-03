f494dbde4134426c30c041a6cfb234915500e27c
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2020-06-03 10:03:48 +0200

    2.addTwoDigits test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,5 @@
 import addBorder from "../src/1.add-border";
+import addTwoDigits from "../src/2.add-two-digits";
 
 describe("addBorder()", () => {
   it("adds a border around entire application", () => {
@@ -13,5 +14,21 @@ describe("addBorder()", () => {
 
     // assert
     expect(result).toEqual(["*****", "*abc*", "*ded*", "*****"]);
+  });
+});
+
+describe("addTwoDigits()", () => {
+  it("take a two digit number and return the sum of their numbers", () => {
+    // arrange
+    const num = 29;
+
+    // act
+    const result = addTwoDigits(num);
+
+    // log
+    console.log("result: ", result);
+
+    // assert
+    expect(result).toBe(11);
   });
 });
