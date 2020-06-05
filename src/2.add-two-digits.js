2fa9c72f4c90ef9a919646fe433e905f16a22008
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2020-06-06 01:05:21 +0200

    3.first-duplicate test

--- src/2.add-two-digits.js
+++ src/2.add-two-digits.js
@@ -1,5 +1,8 @@
-const addTwoDigits = (num) => {
-  const result = num.toString().split("").reduce((acc, cur) => parseInt(acc) + parseInt(cur));
+const addTwoDigits = num => {
+  const result = num
+    .toString()
+    .split("")
+    .reduce((acc, cur) => parseInt(acc) + parseInt(cur));
   return result;
 };
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,6 @@
 import addBorder from "../src/1.add-border";
 import addTwoDigits from "../src/2.add-two-digits";
+import firstDuplicate from "../src/3.first-duplicate";
 
 describe("addBorder()", () => {
   it("adds a border around entire application", () => {
@@ -30,5 +31,35 @@ describe("addTwoDigits()", () => {
 
     // assert
     expect(result).toBe(11);
+  });
+});
+
+describe("firstDuplicate()", () => {
+  it("returns first duplicated value", () => {
+    // arrange
+    const nums = [2, 1, 3, 5, 3, 2];
+
+    // act
+    const result = firstDuplicate(nums);
+
+    // log
+    console.log("result 1: ", result);
+
+    // assert
+    expect(result).toBe(3);
+  });
+
+  it("returns -1 when no duplicated values", () => {
+    // arrange
+    const nums = [2, 1, 3, 5, 4, 6];
+
+    // act
+    const result = firstDuplicate(nums);
+
+    // log
+    console.log("result 2: ", result);
+
+    // assert
+    expect(result).toBe(-1);
   });
 });
