f0e38f17c815cc48742836a626a120a5afcdbe4a
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2020-06-06 18:46:39 +0200

    firstDuplicate creator's solution

--- src/3.first-duplicate.js
+++ src/3.first-duplicate.js
@@ -13,3 +13,22 @@ const firstDuplicate = (nums) => {
 };
 
 export default firstDuplicate;
+
+/*
+function firstDuplicate(nums) {
+  const firstDup = {};
+  // iterate nums
+  for(let num of nums) {
+  // check for a duplicated value
+  if(firstDup.hasOwnProperty(num)) {
+      // return duplicated nums
+      return num;
+  }
+  
+      firstDup[num] = null;
+  }
+  
+  // dont find a duplicate return -1
+  return -1;
+}
+*/
