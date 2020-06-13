8f9bf414c5fb80d812ad26ba01a8068e771c0c63
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2020-06-13 14:31:14 +0200

    Instructor's solution

--- src/4.sum-all-primes.js
+++ src/4.sum-all-primes.js
@@ -18,3 +18,23 @@ const sumAllPrimes = (num) => {
 };
 
 export default sumAllPrimes;
+
+/*
+function sumAllPrimes(num) {
+  let primesTotal = 0;
+  
+  for(let i = 2; i < num; i++) {
+      for(let j = 2; j <= i; j++) {
+          if(i === j) {
+              primesTotal+= i;
+          }
+          
+          if(i % j === 0) {
+              break;
+          }
+      }
+  }
+  
+  return primesTotal;
+}
+*/
