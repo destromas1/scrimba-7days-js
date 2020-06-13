adccad7f1047846ab7f91d659c55d543e438aec9
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2020-06-13 19:59:52 +0200

    makeArrayConsecutive Instructor solution

--- src/6.make-array-consecutive.js
+++ src/6.make-array-consecutive.js
@@ -13,6 +13,18 @@ const makeArrayConsecutive = nums => {
 
 export default makeArrayConsecutive;
 /*
-
+function makeArrayConsecutive(nums) {
+    let count = 0;
+    const sortedNums = nums.sort((a, b) => a - b);
+    const min = nums[0];
+    const max = nums[nums.length - 1];
+    
+    for(let i = min; i < max; i++) {
+        if(!nums.includes(i)) {
+            count++;
+        } 
+    }
+    
+    return count;
 }
 */
