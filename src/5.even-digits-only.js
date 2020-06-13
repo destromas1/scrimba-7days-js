56d00b3ba4e9f372c05aca8ba54deb63e9b2b04b
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2020-06-13 19:28:25 +0200

    evenDigitsOnly Creator's solution

--- src/5.even-digits-only.js
+++ src/5.even-digits-only.js
@@ -1,12 +1,16 @@
 const evenDigitsOnly = number => {
   const digits = number.toString().split("");
   const isAllEven = digits.every(digit => {
-    return digit % 2 === 0;
+    return parseInt(digit) % 2 === 0;
   });
   return isAllEven;
 };
 
 export default evenDigitsOnly;
 /*
-
+function evenDigitsOnly(number) {
+    const digits = number.toString().split('');
+    
+    return digits.every((digit) => parseInt(digit) % 2 === 0);
+}
 */
